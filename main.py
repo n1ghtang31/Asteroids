04cb692d91cf221e0118d418119d80296696d5bf
Craig Rossiter <crossiter0@gmail.com>
2024-11-19 19:39:54 -0600

    delta time added to loop

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.time.Clock()
+    dt = 0
+
+    
+    #stating the game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
